ea56a0d5019ed103aea0655145d2f7bd37ac3d03
Gbenro Adesoye <Gbenro_Adesoye@yahoo.com>
2021-12-24 13:26:57 -0600

    added smooth scroll for all links

--- js/script.js
+++ js/script.js
@@ -13,9 +13,34 @@ yearEl.textContent = currentYear;
 const btnNavEl = document.querySelector(".btn-mobile-nav");
 const headerEL = document.querySelector(".header");
 
-btnNavEl.addEventListener('click', function() {
-  headerEL.classList.toggle('nav-open');
-})
+btnNavEl.addEventListener("click", function () {
+  headerEL.classList.toggle("nav-open");
+});
+
+//Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    //scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    //scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    //close mobile navigation
+    if (link.classList.contains("main-nav-link"))
+      headerEL.classList.toggle("nav-open");
+  });
+});
 
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
